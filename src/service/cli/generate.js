e580abaab93d743a0aec5c8b0948ecda809b3651
RealRather <ira.romia@yandex.ru>
2020-02-08 17:15:00 +0300

    Adds reading data from files

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -9,57 +9,10 @@ const {
 } = require(`../../utils`);
 
 const DEFAULT_COUNT = 1;
-const FILE_NAME = `mocks.json`;
-
-const TITLES = [
-  `Ёлки. История деревьев`,
-  `Как перестать беспокоиться и начать жить`,
-  `Как достигнуть успеха не вставая с кресла`,
-  `Обзор новейшего смартфона`,
-  `Лучше рок-музыканты 20-века`,
-  `Как начать программировать`,
-  `Учим HTML и CSS`,
-  `Что такое золотое сечение`,
-  `Как собрать камни бесконечности`,
-  `Борьба с прокрастинацией`,
-  `Рок — это протест`,
-  `Самый лучший музыкальный альбом этого года`
-];
-
-const SENTENCES = [
-  `Ёлки — это не просто красивое дерево. Это прочная древесина.`,
-  `Первая большая ёлка была установлена только в 1938 году.`,
-  `Вы можете достичь всего. Стоит только немного постараться и запастись книгами.`,
-  `Этот смартфон — настоящая находка. Большой и яркий экран, мощнейший процессор — всё это в небольшом гаджете.`,
-  `Золотое сечение — соотношение двух величин, гармоническая пропорция.`,
-  `Собрать камни бесконечности легко, если вы прирожденный герой.`,
-  `Освоить вёрстку несложно. Возьмите книгу новую книгу и закрепите все упражнения на практике.`,
-  `Бороться с прокрастинацией несложно. Просто действуйте. Маленькими шагами.`,
-  `Программировать не настолько сложно, как об этом говорят.`,
-  `Простые ежедневные упражнения помогут достичь успеха.`,
-  `Это один из лучших рок-музыкантов.`,
-  `Он написал больше 30 хитов.`,
-  `Из под его пера вышло 8 платиновых альбомов.`,
-  `Процессор заслуживает особого внимания. Он обязательно понравится геймерам со стажем.`,
-  `Рок-музыка всегда ассоциировалась с протестами. Так ли это на самом деле?`,
-  `Достичь успеха помогут ежедневные повторения.`,
-  `Помните, небольшое количество ежедневных упражнений лучше, чем один раз, но много.`,
-  `Как начать действовать? Для начала просто соберитесь.`,
-  `Игры и программирование разные вещи. Не стоит идти в программисты, если вам нравится только игры.`,
-  `Альбом стал настоящим открытием года. Мощные гитарные рифы и скоростные соло-партии не дадут заскучать.`
-];
-
-const CATEGORIES = [
-  `Деревья`,
-  `За жизнь`,
-  `Без рамки`,
-  `Разное`,
-  `IT`,
-  `Музыка`,
-  `Кино`,
-  `Программирование`,
-  `Железо`
-];
+const MOCKS_FILE = `mocks.json`;
+const TITLES_PATH = `./data/titles.txt`;
+const CATEGORIES_PATH = `./data/categories.txt`;
+const SENTENCES_PATH = `./data/sentences.txt`;
 
 const PublicInterval = {
   min: new Date().getMonth() - 2,
@@ -81,20 +34,34 @@ const generatePublications = (count, titles, categories, sentences) => (
   }))
 );
 
+const readFile = async (filePath) => {
+  try {
+    const text = await fs.readFile(filePath, `utf8`);
+
+    return text.trim().split(`\n`);
+  } catch (err) {
+    console.error(chalk.red(err));
+    return [];
+  }
+};
+
 module.exports = {
   name: `--generate`,
   async run(args) {
     const [count] = args;
     const publicationCount = Number.parseInt(count, 10) || DEFAULT_COUNT;
 
-    const content = JSON.stringify(generatePublications(publicationCount, TITLES, CATEGORIES, SENTENCES));
+    const titles = await readFile(TITLES_PATH);
+    const categories = await readFile(CATEGORIES_PATH);
+    const sentences = await readFile(SENTENCES_PATH);
+    const content = JSON.stringify(generatePublications(publicationCount, titles, categories, sentences));
 
     try {
-      await fs.writeFile(FILE_NAME, content);
+      await fs.writeFile(MOCKS_FILE, content);
       console.log(chalk.green(`Operation success. File created.`));
     } catch (err) {
 
-      console.error(chalk.red(`Can't write data to file...`));
+      console.error(chalk.red(err));
     }
   }
 };
