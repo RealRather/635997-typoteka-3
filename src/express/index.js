1298d3288859a6d7aef61e56fb7dcd8a4dc5de22
Романова Ирина Андреевна <irina.an.romanova@rt.ru>
2020-04-25 21:08:57 +0300

    Adds my-routes and fixes index.js

--- src/express/index.js
+++ src/express/index.js
@@ -1,13 +1,16 @@
 'use strict';
 
 const express = require(`express`);
-const mainRoutes = require(`./routes/main-routes`);
 const articlesRoutes = require(`./routes/articles-routes`);
+const myRoutes = require(`./routes/my-routes`);
+const mainRoutes = require(`./routes/main-routes`);
 
 const PORT = 8080;
 
 const app = express();
 
-app.use(`/`, mainRoutes);
 app.use(`/articles`, articlesRoutes);
+app.use(`/my`, myRoutes);
+app.use(`/`, mainRoutes);
+
 app.listen(PORT);
